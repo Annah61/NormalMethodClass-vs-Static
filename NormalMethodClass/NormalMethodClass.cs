ded6e8ccead2b45dc4d5acef84ffa935e252189c
Annah61 <annahmoore.morerwa@gmail.com>
2023-09-18 22:47:24 +0200

    Add project files.

---
+++ NormalMethodClass/NormalMethodClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NormalMethodClass
+{
+    internal class NormalMethodClass
+    {
+        public void greeting()
+        {
+            Console.WriteLine("Hi,greetings from normal method,not static");
+        }
+    }
+}
---
+++ NormalMethodClass/StaticMethodClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NormalMethodClass
+{
+    internal class StaticMethodClass
+    {
+        public StaticMethodClass() 
+         {
+            Public static void greetingsStatic()
+
+                Console.WriteLine("Greetings from static method");
+        }
+    }
+}
